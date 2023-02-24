361abbcc32c48e125547cb1e3c298f439763a871
Saksham093 <m.saksham093@gmail.com>
2023-02-24 18:28:36 +0530

    add the my_f function
    my_f is a linear function and tested.

--- src/MyExample.jl
+++ src/MyExample.jl
@@ -2,4 +2,10 @@ module MyExample
 
 # Write your package code here.
 
-end
+include("extra_file.jl")
+
+greet() = print("Hello Saksham!\n") # Demo txt...
+
+export my_f # Now the function 'my_f' is in general use, use olny for public use ...
+
+end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,15 @@
 using MyExample
 using Test
 
+
 @testset "MyExample.jl" begin
     # Write your tests here.
+    MyExample.greet()
+
+
+    # 2x + 4y
+    @test my_f(2, 1) == 8
+    @test my_f(2, 2) == 12
+    @test my_f(2, 3) == 16
+    @test my_f(2, 4) == 20
 end
